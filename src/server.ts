c4e069a5579507f2cc892008f02ab2505a59851a
jpcanto <jplopessk8@gmail.com>
2020-06-03 00:14:28 -0300

    Post method structure

--- src/server.ts
+++ src/server.ts
@@ -2,10 +2,19 @@ import express from 'express';
 
 const app = express();
 
+app.use(express.json());
+
 app.get('/users', (request, response) => {
     response.json({
         user1: 2
     })
+});
+
+app.post('/users', (request, response) => {
+    const data = request.body;
+    const user = { name: data.name, password: data.password }
+
+    return response.json(user);
 });
 
 app.listen(3333);
