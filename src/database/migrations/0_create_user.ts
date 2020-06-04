3dd928935a9d62e32a8137ac47b52bde1c67a5f6
jpcanto <jplopessk8@gmail.com>
2020-06-04 13:19:51 -0300

    Some notes about keys and migrations config

--- src/database/migrations/0_create_user.ts
+++ src/database/migrations/0_create_user.ts
@@ -5,11 +5,17 @@ export async function up(knex: Knex) {
     return knex.schema.createTable('users', table => {
         // increments é um método nativo onde é incrementado automáticamente a cada criação na tabela.
         // primary seta o campo ID como chave primária.
+
+        // Exemplo de criação de chave estrangeira:
+        // table.integer('nome do campo').notNullable().references('nome do campo na outra tabela').inTable('nome da tabela')
+
         table.increments('id').primary();
-        table.string('name').notNullable();
-        table.string('email').notNullable();
+        table.string('name').unique().notNullable();
+        table.string('email').unique().notNullable();
         table.string('password', 10).notNullable();
         table.string('image');
+        table.string('games');
+        table.string('characters').unique();
     });
 };
 
