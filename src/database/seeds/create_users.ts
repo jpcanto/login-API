06ea5ade2ec0226a0efa63cfe6b788feb5c8a696
jpcanto <jplopessk8@gmail.com>
2020-06-05 14:44:49 -0300

    some adjustments (clean code)

--- src/database/seeds/create_users.ts
+++ src/database/seeds/create_users.ts
@@ -2,15 +2,15 @@ import Knex from 'knex';
 
 // seeds criam automáticamente instancias na tabela ao rodar a aplicação, é usado quando precisamos que já seja consumido da api ao iniciar a aplicação.
 export async function seed(knex: Knex) {
-    knex('users').insert([
+    await knex('users').insert([
         {
-            'id': 'id_exemple',
-            'name': 'name_exemple',
-            'email': 'email_exemple',
-            'password': 'password_exemple',
-            'image': 'image_exemple',
-            'games': 'games_exemple',
-            'characters': 'characters_exemple'
+            id: 1,
+            name: 'name_exemple',
+            email: 'email_exemple',
+            password: 'password_exemple',
+            image: 'image_exemple',
+            games: 'games_exemple',
+            characters: 'characters_exemple'
         }
     ]);
 };
--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,12 @@
 import express from 'express';
+import knex from './database/connection';
 
 const routes = express.Router();
 
-routes.get('/', (request, response) => {
-    return response.json({message: 'hello world'});
+routes.get('/users', async (request, response) => {
+    const users = await knex('users').select('*');
+    // knex equivalente á: "SELECT * FROM users" de sql language
+    return response.json(users);
 });
 
 export default routes;
